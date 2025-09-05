eb4ffeb33dce61d58e2ac343a23eb20a607cf05a
Dmitrii <wonderrfau1t@gmail.com>
2025-09-05 14:23:25 +0700

    Added tg bot for emulating random scenario

---
+++ bot/db/database.py
@@ -0,0 +1,32 @@
+from aiogram import BaseMiddleware
+from sqlalchemy.orm import Session
+from typing import Callable, Dict, Any, Awaitable
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from os import getenv
+
+DB_USER = getenv("DB_HOST")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
+DB_NAME = getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class DatabaseMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[Any, Dict[str, Any]], Awaitable[Any]],
+        event: Any,
+        data: Dict[str, Any]
+    ) -> Any:
+        db: Session = SessionLocal()
+        try:
+            data["db"] = db  # прокидываем в handler через data
+            return await handler(event, data)
+        finally:
+            db.close()
